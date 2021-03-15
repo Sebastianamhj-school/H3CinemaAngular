8056d35e9fb854ac79ec8f905cbcce0a1274b295
Sebastianamhj-school <78901791+Sebastianamhj-school@users.noreply.github.com>
2021-03-15 15:20:28 +0100

    navbar to sticky movie-poster styling

--- src/app/Components/movie-poster/movie-poster.component.ts
+++ src/app/Components/movie-poster/movie-poster.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/Models/Movie';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-movie-poster',
@@ -11,9 +12,12 @@ export class MoviePosterComponent implements OnInit {
   @Input()
   movie: Movie;
   
-  constructor() { }
+  constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
   }
 
+  getTheme() {
+    return this.themeService.isDarkMode;
+  }
 }
