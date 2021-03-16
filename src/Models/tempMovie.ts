ebd4f4a05a5affa69389b3337d794db50bc9e0a4
Nicolai Friis <nfriis24@gmail.com>
2021-03-16 09:15:55 +0100

    Working on booking-seat component

--- src/Models/tempMovie.ts
+++
@@ -1,9 +0,0 @@
-export class tempMovie {
-    public Id: number;
-    public Title: string;
-    public Runtime: number;
-    public Rating: number;
-    public AgeRating: string;
-    public ImgUrl: string;
-    public ReleaseDate: Date;
-}
