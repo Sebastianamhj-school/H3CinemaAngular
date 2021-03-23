cb1f7a64845d70eed8ba832ab6b03f8801327048
Sebastianamhj-school <78901791+Sebastianamhj-school@users.noreply.github.com>
2021-03-23 16:46:15 +0100

    update screening component and styling

--- src/app/pages/screening/screening.component.ts
+++ src/app/pages/screening/screening.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { APIService } from 'src/app/services/api.service';
 import { ThemeService } from 'src/app/services/theme.service';
 import { Screening } from 'src/Models/Screening';
+import { Seat } from 'src/Models/Seat';
 
 @Component({
   selector: 'app-screening',
@@ -14,6 +15,8 @@ export class ScreeningComponent implements OnInit {
   id: number;
   screening: Screening;
   tickets: number = 2;
+  selectedSeats: Seat[] = [];
+  maxSeats: number;
 
   constructor(
     private _Activatedroute: ActivatedRoute,
@@ -28,8 +31,9 @@ export class ScreeningComponent implements OnInit {
 
     this.apiService.getScreeningsById(this.id).subscribe(dataAPI => {
       this.screening = dataAPI;
-      console.log(this.screening);
+      this.maxSeats = Math.max.apply(Math, this.screening.seats.map(function (o) { return o.seatNumber; }))
     });
+
   }
 
   getTheme() {
@@ -37,15 +41,19 @@ export class ScreeningComponent implements OnInit {
   }
 
   addTicket() {
-    this.tickets++;
+    if (this.tickets != this.maxSeats) {
+      this.tickets++;
+    }
   }
 
   minusTicket() {
-    if (this.tickets == 0) {
-      alert("Ticket can't be below 0");
-    } else {
+    if (this.tickets != 0) {
       this.tickets--;
     }
+  }
+
+  getSeats(event) {
+    this.selectedSeats = event;
   }
 
 }
