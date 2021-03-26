78b7d580159cea160ed0cfe413ec557bf8b02b16
Sebastianamhj-school <78901791+Sebastianamhj-school@users.noreply.github.com>
2021-03-26 15:43:00 +0100

    add admin initial styling

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
+  }
+
+  getTheme() {
+    return this.themeService.isDarkMode;
   }
 
 }
