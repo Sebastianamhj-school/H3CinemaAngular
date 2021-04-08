285cbed4e057bccb29c385c7240cd7cc838c8838
Sebastianamhj-school <78901791+Sebastianamhj-school@users.noreply.github.com>
2021-04-08 11:52:14 +0200

    admin dynamic form

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { APIService } from 'src/app/services/api.service';
 import { ThemeService } from 'src/app/services/theme.service';
 import { AutoComplete } from 'src/Models/AutoComplete';
@@ -18,14 +19,37 @@ export class AdminComponent implements OnInit {
     ['Bookings', 'square']
   ];
 
+  movieFormGroup = this.fb.group({
+    title: [''],
+    runtime: [''],
+    rating: [''],
+    ageRating: [''],
+    imgUrl: [''],
+    releaseDate: [''],
+    genre: this.fb.array([
+      this.fb.control('')
+    ]),
+    description: [''],
+  });
+
   activeNavigation: string[] = this.navigationItems[0];
   movieId: number;
-  
+  status: string;
 
-  constructor(private themeService: ThemeService, private apiService: APIService) { }
+  constructor
+  (
+    private themeService: ThemeService,
+    private apiService: APIService,
+    private fb: FormBuilder
+  ) {
+  }
 
   ngOnInit(): void {
     
+  }
+
+  exitCreate() {
+    this.status = "null";
   }
 
   getTheme() {
@@ -33,7 +57,28 @@ export class AdminComponent implements OnInit {
   }
 
   onNavigationItemClick(i: number) {
+    this.status = "null";
     this.activeNavigation = this.navigationItems[i];
+  }
+
+  receiveBoxClicked(event) {
+    this.status = event;
+  }
+
+  addGenre() {
+    this.genres.push(this.fb.control(''))
+  }
+
+  removeGenre(genreIndex: number) {
+    this.genres.removeAt(genreIndex);
+  }
+
+  get genres(): FormArray {
+    return this.movieFormGroup.get("genre") as FormArray;
+  }
+
+  movieSubmit() {
+    console.log(this.movieFormGroup.get("genre")["controls"]);
   }
 
 }
